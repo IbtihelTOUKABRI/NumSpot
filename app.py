6d32afcc3401c6bea715d8836c9e0ecd4db66e9d
IbtihelTOUKABRI <ibtihel11toukabri@gmail.com>
2023-03-08 17:12:59 +0100

    modifier app.py et liste_goodies.html

--- app.py
+++ app.py
@@ -1,124 +1,62 @@
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-## Module name : app.py
-## Author :      ITO
-## Description : Distribution des goodies 
-##          
-## History :     Date         | By  | Modification
-##               07/03/2023   | ITO | Init version
-##               08/03/2023   | ITO | Changer le log dans la sortie standard , modifier la path de la BD
-##               08/03/2023   | ITO | Changer la BD par un dict             
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
-import sqlite3
 from datetime import datetime
-from flask import *
+from flask import Flask, render_template
 import random
 
-#####################
+stock_init = {   
+    'gourde': 50, 
+    'stylo': 100, 
+    'support ordi': 150, 
+    'balle': 200,
+    'support bambou': 300,
+    'pince': 300,
+    'cache webcam': 400
+    }
 
+stock = stock_init.copy()
+goodies_p = [i/sum(stock.values()) for i in list(stock.values())]
 
 app = Flask(__name__)
 
-stock = {
-    'gourde': 1,
-    'stylo': 1,
-    'support_ordi': 1,
-    'balle': 1,
-    'support_bambou': 1,
-    'pince': 1,
-    'cache': 1 }
-
-
 @app.route("/")
-def list_goodies ():
-
-    try:
-        
-        print(stock)
-        
-        return render_template("list_goodies.html",goodies=stock)
-
-    except Exception as e:
-        print('erreur listing goodies')
-        print(e)
-
-        return "erreur listing goodies"
+def list_goodies():
+    global stock
+    return render_template("list_goodies.html", goodies=list(stock.values()))
   
 ###############################################
 ## New Tirage
 ###############################################
 @app.route("/tirage", methods =['POST','GET'])
 def tirage ():
+    global stock
+    global goodies_p
+    if sum(stock.values()) > 0:
+        item = random.choices(list(stock.keys()), goodies_p)[0]
+        stock[item] -= 1
 
-    try:
-        
-        
-        print("debut tirage")
+        if stock[item] == 0:
+            # recalcul des probas avec le stock restant
+            # on ajoute 1 au dénominateur pour éviter un /0 quand le dernier item est pioché
+            goodies_p = [i/(sum(stock.values())+1) for i in list(stock.values())]
 
+        print(f'{stock}')
+        return render_template("tirage.html", article=item)
 
-        goodies_p = [i/sum(stock.values()) for i in list(stock.values())]
-
-        print(f'goodies_p: {goodies_p}')
-
-        
-
-        while sum(stock.values()) > 0:
-
-            print(f'sum(stock.values(): {sum(stock.values())}')
-
-            
-            item = random.choices(list(stock.keys()), goodies_p)[0]
-
-            print(f'item: {item}')
-            
-
-            stock[item] -= 1
-            if stock[item] == 0:
-                # recalcul des probas avec le stock restant
-                # on ajoute 1 au dénominateur pour éviter un /0 quand le stock est vide
-                goodies_p = [i/(sum(stock.values())+1) for i in list(stock.values())]
-                print(f'probas: {goodies_p}')
-                print(f'tirage item: {item}, stock restant: {stock}, total: {sum(stock.values())}')
-                if(sum(stock.values()) > 0):
-                    return render_template("tirage.html",article=item)
-                else:
-                    
-                    return render_template("list_goodies.html",goodies=stock)    
- 
-    except Exception as e:
-        print("erreur tirage")
-        
-        print(e)
-
-        return "erreur Tirage"
+    else:
+        print(f'Stock is empty !')
+        return render_template("tirage.html", article='Vide !')
+    
 
 ###############################################
 ## Remise à zéro
 ###############################################
 @app.route("/raz", methods =['POST','GET'])
 def raz ():
+    global stock
+    global stock_init
+    
+    stock = stock_init.copy()
 
-    try:
-        stock = {
-            'gourde': 1,
-            'stylo': 1,
-            'support_ordi': 1,
-            'balle': 1,
-            'support_bambou': 1,
-            'pince': 1,
-            'cache': 1 }
-
-        print(stock)
-        print("raz OK")
-        
-        return render_template("list_goodies.html",goodies=stock)
-    except Exception as e:
-        print("erreur Remise à zero")
-
-        print(e)
-
-        return "erreur Remise à zero"
+    return render_template("list_goodies.html", goodies=list(stock.items()))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=False)
-
+     app.run(host='0.0.0.0', port=8080, debug=True)
